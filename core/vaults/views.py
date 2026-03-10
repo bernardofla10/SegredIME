1ae685023bf69704eab3d256be0b10d09a3e4776
Thiago Angelo <thiago.angelo.oliveira@gmail.com>
2026-03-09 22:35:25 -0300

    django_v2

--- core/vaults/views.py
+++ core/vaults/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.utils import timezone
+from django.http import JsonResponse
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def status_view(request):
+    return JsonResponse({
+        "status": "ok",
+        "service": "segredime"
+    })
